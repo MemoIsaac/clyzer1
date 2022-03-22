86341cce31fd1ec72c265b67da221809372987f8
MemoIsaac <102159189+MemoIsaac@users.noreply.github.com>
2022-03-22 16:02:42 +0300

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,46 @@
+const db = require("nrc.db");
+
+module.exports = async(client) => {
+
+  
+  client.user.setStatus("idle");
+  const aktiviteler = [
+    "c!yardım",
+  ]
+
+
+
+  
+  
+  console.log("Ayarlamalar: Durum Ayarlandı!")
+  console.log("Ayarlamalar: Aktivite Ayarlandı!")
+  
+  console.log(" ")
+
+
+
+  const kulsayi = []
+  client.guilds.cache.forEach((item, i) => {
+      kulsayi.push(item.memberCount)
+  });
+  var toplamkulsayi = 0
+  for (var i = 0; i < kulsayi.length; i++) {
+      if (isNaN(kulsayi[i])){
+          continue;
+      }
+
+      toplamkulsayi += Number(kulsayi[i])
+  }
+
+  console.log("Bot İstatistiği")
+  console.log(`Sunucu Sayısı: ${client.guilds.cache.size}`)
+  console.log(`Kullanıcı Sayısı: ${toplamkulsayi}`)
+  console.log(`Emoji Sayısı: ${client.emojis.cache.size}`)
+  console.log(`Kanal Sayısı: ${client.channels.cache.size}`)
+
+  console.log(" ")
+
+  console.log(`${client.user.tag} olarak Discord'a giriş yaptım. Artık kullanmaya hazırım!\nİyi Kullanımlar Dilerim...`);
+
+
+}
